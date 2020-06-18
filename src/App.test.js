68d514c50e839edad91a31cfe2564674ecda88e5
Callum Hart <mini-ninja-64@users.noreply.github.com>
2020-06-18 16:04:59 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import {cleanup, fireEvent, render} from '@testing-library/react';
 
 test('Expected Text is displayed', async () => {
     const {findAllByText} = render(<App/>);
-       
+
     const ratingLegend = await findAllByText(/What would you rate our service:/);
     expect(ratingLegend.length).toStrictEqual(1);
 });
