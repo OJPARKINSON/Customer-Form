d176e07c616d690037d7ab7f34d4ca44256ca165
TheReconJacob <jvpokeyjake@gmail.com>
2020-06-18 14:47:18 +0100

    fixed css

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ function App() {
         <form>
           <div id="rating">
             <label for="rating">What would you rate our service:</label>
-            <label class="fa fa-star" for="1"></label>
             <input name="ratingInput" class="fa fa-star" id="1" type="radio" value="1" />
-            <label class="fa fa-star" for="2"></label>
+            <label class="fa fa-star" for="1"></label>
             <input name="ratingInput" id="2" type="radio" value="2" />
+            <label class="fa fa-star" for="2"></label>
+            <input name="ratingInput" id="3" type="radio" value="3" />
             <label class="fa fa-star" for="3"></label>
-            <input name="ratingInput" id="3" type="radio" value="3"/>
-            <label class="fa fa-star" for="4"></label>
             <input name="ratingInput" id="4" type="radio" value="4" />
+            <label class="fa fa-star" for="4"></label>
+            <input name="ratingInput" id="5" type="radio" value="5" />
             <label class="fa fa-star" for="5"></label>
-            <input name="ratingInput" id="5" type="radio" value="5" checked />
           </div>
           <div>
             <label for="feedback">Type your thoughts here:</label>
