b6824a89c498b3047983edd9cfaee2773d35b0e4
mini-ninja-64 <callumhart@btinternet.com>
2020-06-18 10:44:16 +0100

    Add react components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import { TextareaAutosize } from '@material-ui/core';
-// import { Rating } from '@material-ui/lab';
+import { Rating } from '@material-ui/lab';
 
 function App() {
   return (
     <div className="App">
       <form>
         <Rating/>
-        {/* <TextareaAutosize></TextareaAutosize> */}
+        <TextareaAutosize></TextareaAutosize>
       </form>
     </div>
   );
