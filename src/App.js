487179749c6d51d8f4c0ae23db13641615e86d8c
Oliver <olliepar01@gmail.com>
2020-06-18 11:28:25 +0100

    moved code accross

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import './style.css'
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        <form>
+          <div id="rating">
+            <label for="rating">What would you rate our service:</label>
+            <label class="fa fa-star" for="1"></label>
+            <input name="ratingInput" class="fa fa-star" id="1" type="radio" value="1" />
+            <label class="fa fa-star" for="2"></label>
+            <input name="ratingInput" id="2" type="radio" value="2" />
+            <label class="fa fa-star" for="3"></label>
+            <input name="ratingInput" id="3" type="radio" value="3"/>
+            <label class="fa fa-star" for="4"></label>
+            <input name="ratingInput" id="4" type="radio" value="4" />
+            <label class="fa fa-star" for="5"></label>
+            <input name="ratingInput" id="5" type="radio" value="5" checked />
+          </div>
+          <div>
+            <label for="feedback">Type your thoughts here:</label>
+            <textarea id="feedback" rows="4" cols="50"></textarea>
+
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }
 
